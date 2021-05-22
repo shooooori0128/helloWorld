e6a51c69983e16cbf88437628f87b7d89306c340
shooooori0128 <pf.shori0128@gmail.com>
2021-05-22 10:48:48 +0900

    メッセージの修正

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 function helloWorld(req: any, res: any) {
-  const message: string = req.query.message || req.body.message || 'Hello Shooooorisan!';
+  const message: string = req.query.message || req.body.message || 'Hello Nishizawa Shooooorisan!';
   res.status(200).send(message);
 }
 
