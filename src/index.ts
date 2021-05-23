9e7e56af737d5544640027b5894673c5c4409ea2
shooooori0128 <pf.shori0128@gmail.com>
2021-05-23 10:38:09 +0900

    メッセージの修正

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 function helloWorld(req: any, res: any) {
-  const message: string = req.query.message || req.body.message || 'Hello ふがががががが!';
+  const message: string = req.query.message || req.body.message || 'Hello もげげげげげげ!';
   res.status(200).send(message);
 }
 
